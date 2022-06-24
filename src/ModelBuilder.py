22b9b33864b4174d277a32d374ee4e308a59b707
MarianaCast <mxc160930@utdallas.edu>
2022-06-24 15:34:26 -0400

    test commit

---
+++ src/ModelBuilder.py
@@ -0,0 +1,70 @@
+#Import TensorFlow Keras Libary to create a CNN (Convolutional Neural Network)
+import tensorflow as tf
+from sklearn.utils import shuffle
+import numpy as np
+from glob import glob
+import matplotlib.pyplot as plt
+from tensorflow.keras.layers import MaxPooling2D
+import time
+import pickle
+
+
+#TODO: Separate the individual params for easier model modification
+class ModelBuilder:
+    
+    def create_model():
+        model = tf.keras.Sequential([
+            tf.keras.layers.Conv2D(32, (3,3), activation='relu',input_shape=(224,224,3)),
+            tf.keras.layers.MaxPooling2D(2,2),
+            tf.keras.layers.Conv2D(32,(3,3), activation='relu'),
+            tf.keras.layers.MaxPooling2D(2,2),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(128, activation=tf.nn.relu),
+            tf.keras.layers.Dense(15, activation=tf.nn.softmax)
+        ])
+
+        return model
+    
+    def compile_model(model):
+        model.compile(loss="sparse_categorical_crossentropy" ,optimizer="adam", metrics=["accuracy"])
+
+        return model
+
+    def fit_model(model, training_set, test_set, training_labels):
+        history = model.fit(training_set, training_labels, batch_size=128, epochs=15, validation_split=0.2)
+
+        return history
+    def pickle_history(history):
+        #Save the history
+        with open('src/history', 'wb') as file:
+            pickle.dump(history.history, file)
+
+    def save_model(model):
+        timestr = time.strftime("%Y%m%d-%H%M%S")
+        model.save('models/model-%s',timestr)
+
+    def load_history():
+        history = pickle.load(open('src/history.json', "rb"))
+        return history
+
+    def visualize_model(history):
+        figure = plt.figure(figsize=(10,5))
+
+        plt.subplot(221)
+        plt.plot(history['accuracy'],'bo--', label="acc")
+        plt.plot(history['val_accuracy'], 'ro--', label="val_access")
+        plt.title("train_acc vs val_acc")
+        plt.ylabel("accuracy")
+        plt.xlabel("epochs")
+        plt.legend()
+
+        #Loss Function
+        plt.subplot(222)
+        plt.plot(history['loss'],'bo--', label="loss")
+        plt.plot(history['val_loss'], 'ro--', label="val_loss")
+        plt.title("train_loss vs val_loss")
+        plt.ylabel("loss")
+        plt.xlabel("epochs")
+        plt.legend()
+
+        plt.show()
--- src/main.py
+++ src/main.py
@@ -2,20 +2,91 @@ import glob
 import cv2
 import matplotlib.pyplot as plt
 import os
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import sys
+from ModelBuilder import ModelBuilder
+import argparse
+ 
+parser = argparse.ArgumentParser()
+parser.add_argument("-t","--Train", help="Show Output")
+parser.add_argument("-v","--Visualize", help="Show Output")
+parser.add_argument("-lm", "--Load_Model", help="Show Output")
+parser.add_argument("-d", "--Display", help="Show Output")
+args = parser.parse_args()
 
+#TODO: Actually split up the program or present users with options
 def main():
-    print("Bug Identifying Model")
-    read_folder()
+    if args.Train:
+        print('Setting up train and test sets')
+        train_images,train_labels,test_images,test_labels = create_train_test()
 
-def read_folder():
+        # train_images, train_labels = shuffle(train_images,train_labels, random_state=25)
+        print('Create CNN Model')
+        model = ModelBuilder.create_model()
+        model = ModelBuilder.compile_model(model)
+        history = ModelBuilder.fit_model(model, train_images, test_images, train_labels)
+
+        print('Saving Model')
+        ModelBuilder.save_model(model)
+
+        ModelBuilder.pickle_history(history)
+
+    if args.Load_Model:
+        history = ModelBuilder.load_history()
+
+    if args.Display:
+        print('Displaying model history')
+        ModelBuilder.visualize_model(history)
+
+def create_train_test():
+    spiders = ["Black Widow", "Blue Tarantula", "Bold Jumper", "Brown Grass Spider", "Brown Recluse Spider",
+        "Deinopis Spider", "Golden Orb Weaver", "Hobo Spider", "Huntsman Spider", "Ladybird Mimic Spider",
+        "Peacock Spider", "Red Knee Tarantula", "Spiny-backed Orb-weaver", "White Kneed Tarantula", "Yellow Garden Spider"]
+
+    spiders_label = {spiders: i for i, spiders in enumerate(spiders)}
+
+    train_labels = []
+    test_labels = []
+    test_images = []
+    test_file_name = []
+    train_images = []
+    train_file_name = []
+
     #Set up the folder names
-    train_folder = "../Data/train/Black Widow"
-    test_folder = "../Data/test"
+    train_folder = "Data/train"
+    test_folder = "Data/test"
 
-    #Read the test folder
-    images = [cv2.imread(file) for file in glob.glob("Data/train/*/*.jpg")]
-    thing = images[0]
-    cv2.imshow("spider",thing)
+    #Read the train list with the label list
+    train_folders = os.listdir(train_folder)
+
+    for folder in train_folders:
+        image_path = os.path.join(train_folder,folder)
+        for file in glob.glob(os.path.join(image_path,"*.jpg")):
+            train_labels.append(spiders_label[folder])
+            train_images.append(cv2.imread(file))
+            train_file_name.append(file)
+
+    test_folders = os.listdir(test_folder)
+    for folder in test_folders:
+        image_path = os.path.join(test_folder,folder)
+        for file in glob.glob(os.path.join(image_path,"*.jpg")):
+            test_labels.append(spiders_label[folder])
+            test_images.append(cv2.imread(file))
+            test_file_name.append(file)
+
+
+    train_images = np.array(train_images, dtype='float32')
+    test_images = np.array(train_images, dtype='float32')
+
+    train_labels = np.array(train_labels, dtype='int32')
+    test_labels = np.array(test_labels, dtype='int32')
+    # cv2.imshow(train_labels[0],train_img[0])
+    # cv2.waitKey(0)
+    
+    return train_images,train_labels,test_images,test_labels
+
 
 if __name__ == '__main__':
     main()
